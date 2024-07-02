31848a5f90f6de1fab909cd198ca2061e64d98a7
Jarlyyn <jarlyyn@qq.com>
2024-07-02 16:39:38 +0800

    version

--- lib/states/appstate.dart
+++ lib/states/appstate.dart
@@ -16,7 +16,7 @@ late AppState currentAppState;
 
 class AppState extends ChangeNotifier {
   bool inGame = false;
-  String version = "0.24.06.27 [API 1.24.06.23]";
+  String version = "0.24.07.02 [API 1.24.06.23]";
   var navigatorKey = GlobalKey<NavigatorState>();
   Config config = Config();
   String settingsPath = "";
